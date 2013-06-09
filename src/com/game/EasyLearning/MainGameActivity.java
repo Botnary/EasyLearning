a74e39506ac2c77f0904911983b07d037e6fa269
Constantin Botnari <write.4k@gmail.com>
2013-06-09 15:31:50 -0400

    MainGameActivity added

---
+++ src/com/game/EasyLearning/MainGameActivity.java
@@ -0,0 +1,28 @@
+package com.game.EasyLearning;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+public class MainGameActivity extends BaseGameActivity {
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;  //TODO
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+        //TODO
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+        //TODO
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+        //TODO
+    }
+}
